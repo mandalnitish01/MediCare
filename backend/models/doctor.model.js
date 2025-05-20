fcc74b62cd4947a3cf6481c571721e2bae828ee7
macOS <vibhu@192.168.2.104>
2025-05-21 02:10:08 +0530

    configred cloudnary and created models

---
+++ backend/models/doctor.model.js
@@ -0,0 +1,22 @@
+import mongoose, { mongo } from "mongoose";
+
+const doctorSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  emial: { type: String, unique: true, required: true },
+  password: { type: String, required: true },
+  image: { type: String, required: true },
+  speciality: { type: String, required: true },
+  degree: { type: String, required: true },
+  experience: { type: String, required: true },
+  about: { type: String, required: true },
+  available: { type: Boolean, required: true },
+  fees: { type: Number, required: true },
+  address: { type: Object, required: true },
+  date: { type: Number, required: true },
+  slot_booked: { type: Object, default: {} }
+  
+}, { minimize: false });
+
+const doctorModel = mongoose.models.doctor || mongoose.model('doctor', doctorSchema);
+
+export default doctorModel;
